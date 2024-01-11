86a98d4e826dfbc72b34ac805f85e0b9063f2198
evandrolucas60 <evandrolucas60@yahoo.com>
2024-01-11 16:55:44 -0300

    Writing the drag and drop test

--- 15_User_Interactions/15_User_Interactions/InteractionsDemo.cs
+++ 15_User_Interactions/15_User_Interactions/InteractionsDemo.cs
@@ -2,7 +2,9 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System.Reflection;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
 
 namespace _15_User_Interactions
 {
@@ -14,6 +16,14 @@ namespace _15_User_Interactions
         public void Exemple1()
         {
             var actions = new Actions(_driver);
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(6));
+
+            _driver.Navigate().GoToUrl("https://jqueryui.com/droppable/");
+            wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
+
+
+
+            _driver.Quit();
         }
 
         private static IWebDriver GetChromeDrive()
